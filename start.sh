a3550dc2daf8b70262a8ccd8393f1e94b5850332
mymusise <mymusise1@gmail.com>
2020-10-21 16:21:14 +0800

    fix
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-rm /data/finetune_model/*
+rm /data/gpt2-ml-finetune/*
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$(pwd)  python3 train/train_wc.py --input_file=dataset/train_data/train.tfrecord --output_dir=/data/gpt2-ml-finetune --init_checkpoint=/data/gpt2-ml-model/model.ckpt-220000
